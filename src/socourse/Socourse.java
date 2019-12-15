b897d9451cb7ee3ce39e08872bf654f7d63ced25
Satria Aji Putra <satriamaxt@gmail.com>
2019-12-15 11:23:32 +0700

    Initial commit

---
+++ src/socourse/Socourse.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socourse;
+
+/**
+ *
+ * @author Satria Aji Putra
+ */
+public class Socourse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        RegisterFrame registerFrame = new RegisterFrame();
+        registerFrame.setVisible(true);
+    }
+    
+}
